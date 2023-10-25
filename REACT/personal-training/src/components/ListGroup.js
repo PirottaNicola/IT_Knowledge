06cce1f3868667404cd44528b697a2c6f5cdca30
Nicola Pirotta <nicolapirotta@gmail.com>
2023-10-25 16:23:32 +0200

    passing props to child component, useState()

--- REACT/personal-training/src/components/ListGroup.js
+++ REACT/personal-training/src/components/ListGroup.js
@@ -1,31 +1,43 @@
-function ListGroup() {
-    // data
-    const items = [
-        'An item',
-        'A second item',
-        'A third item',
-        'A fourth item',
-        'And a fifth one',
-    ]
+import { useState } from 'react'
 
-    // event handlers
-    const handleClick = (event) => {
-        console.log(event.target.innerText);
-        console.log(`clicked on item "${event.target.innerText}"`)
-    }
+function ListGroup({ items, heading }) {
+  // destructuring props. If not, we would have to use props.items and props.heading
 
-    // render
-    return (
-        <>
-            <h1>List</h1>
-            <ul className='list-group'>
-                {items.length < 1 ? <p>Nothing in the list</p> : items.map((item, index) => (
-                    <li key={item} className='list-group-item' onClick={handleClick}>{item}
-                    </li> // key is a unique identifier for each item, in this case we use the item itself cause we know it's unique, but we can use an id if we have one
-                ))}
-            </ul>
-        </>
-    )
+  // Hooks
+  const [selectedIndex, setSelectedIndex] = useState(-1) // -1 means no item is selected at the beginning
+
+  // event handlers
+  const handleClick = (event) => {
+    // highlight selected item
+    setSelectedIndex(event.target.dataset.index)
+  }
+
+  // render
+  return (
+    <>
+      <h1>{heading}</h1>
+      <ul className='list-group'>
+        {items.length < 1 ? (
+          <p>Nothing in the list</p>
+        ) : (
+          items.map((item, index) => (
+            <li
+              key={item}
+              data-index={index}
+              className={
+                selectedIndex == index
+                  ? 'list-group-item active'
+                  : 'list-group-item'
+              }
+              onClick={handleClick}
+            >
+              {item}
+            </li> // key is a unique identifier for each item, in this case we use the item itself cause we know it's unique, but we can use an id if we have one
+          ))
+        )}
+      </ul>
+    </>
+  )
 }
 
 export default ListGroup
