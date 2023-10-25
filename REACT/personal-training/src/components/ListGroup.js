2409b7aa48a6da0540b79b6a1b576588d7eea4b2
Nicola Pirotta <nicolapirotta@gmail.com>
2023-10-25 14:00:57 +0200

    adding event handling

--- REACT/personal-training/src/components/ListGroup.js
+++ REACT/personal-training/src/components/ListGroup.js
@@ -1,4 +1,5 @@
 function ListGroup() {
+    // data
     const items = [
         'An item',
         'A second item',
@@ -6,12 +7,21 @@ function ListGroup() {
         'A fourth item',
         'And a fifth one',
     ]
+
+    // event handlers
+    const handleClick = (event) => {
+        console.log(event.target.innerText);
+        console.log(`clicked on item "${event.target.innerText}"`)
+    }
+
+    // render
     return (
         <>
             <h1>List</h1>
             <ul className='list-group'>
-                {items.length < 1 ? <p>Nothing in the list</p> : items.map((item) => (
-                    <li key={item} className='list-group-item'>{item}</li> // key is a unique identifier for each item, in this case we use the item itself cause we know it's unique, but we can use an id if we have one
+                {items.length < 1 ? <p>Nothing in the list</p> : items.map((item, index) => (
+                    <li key={item} className='list-group-item' onClick={handleClick}>{item}
+                    </li> // key is a unique identifier for each item, in this case we use the item itself cause we know it's unique, but we can use an id if we have one
                 ))}
             </ul>
         </>
